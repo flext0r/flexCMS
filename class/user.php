541bf23c669c0dff26799b95cf093742ac217ace
flext0r <maxxv100@gmail.com>
2016-12-17 17:28:09 +0100

    recreated register function

--- class/user.php
+++ class/user.php
@@ -44,50 +44,57 @@ class User
 		if($Error != ''){ return $Error;}
 	}
 		
-		
-	public function Register($login,$password,$password2,$email)//needs to be reworked to show all the errors
+
+	public function Register($login,$password,$password2,$email)
 	{
 			$Error = [];
 			if(empty($login) OR empty($password) OR empty($password2) OR empty($email))
 			{
-				$Error = "Form can't be empty!";
-			}else{
-				$SQL = $this->db->prepare("SELECT user_login FROM users WHERE user_login = :login LIMIT 1");
+				$Error[] = "Form can't be empty!";
+				
+			}
+			if($password != $password2)
+			{
+				$Error[] = "Passwords don't match!";
+			}
+			if (filter_var($email, FILTER_VALIDATE_EMAIL) == false) 
+			{
+				$Error[] = "Email is invalid!";
+			}
+				$SQL = $this->db->prepare("SELECT user_login,user_email FROM users WHERE user_login = :login AND user_email = :email LIMIT 1");
 				$SQL->bindParam('login',$login);
+				$SQL->bindParam('email',$email);
 				$SQL->execute();
 				$Row = $SQL->fetch(PDO::FETCH_ASSOC);
 				if($Row['user_login'] == $login)
 				{
 					$Error[] = "Login <b>".$login."</b> already exist!";
-				}else{
-					if($password != $password2)
-					{
-						$Error[] = "Passwords don't match!";
-					}else{
-						if (filter_var($email, FILTER_VALIDATE_EMAIL) == false) 
-						{
-							$Error[] = "Email is invalid!";
-						}else{
-						try{
-							$date = date("Y-m-d H:i:s");
-							$password = $this->hashpass($password);
-							$SQL = $this->db->prepare("INSERT INTO users (user_login,user_password,user_email,user_date) VALUES (:login,:password,:email,:date)");
-							$SQL->bindParam(':login',$login,PDO::PARAM_STR);
-							$SQL->bindParam(':password',$password,PDO::PARAM_STR);
-							$SQL->bindParam(':email',$email,PDO::PARAM_STR);
-							$SQL->bindParam(':date',$date,PDO::PARAM_STR);
-							$SQL->execute();
-							echo '<script>alert("Your account '.$login.' has been created!");</script>';
-							}catch(PDOEXCEPTION $e)
-							{
-								echo $e->getMessage();
-							}
-						}
-					}	
 				}
-			if(count($Error) > 0){return $Error;}
-		}
+				if($Row['user_email'] == $email)
+				{
+					$Error[] = 'Email <b>'.$email.'</b> is already taken!';
+				}
+				if(count($Error) == 0)
+				{
+					try{
+						$datee = date("Y-m-d H:i:s");
+						$password = $this->hashpass($password);
+						$SQL = $this->db->prepare("INSERT INTO users (user_login,user_password,user_email,user_date) VALUES (:login,:password,:email,:date)");
+						$SQL->bindParam(':login',$login,PDO::PARAM_STR);
+						$SQL->bindParam(':password',$password,PDO::PARAM_STR);
+						$SQL->bindParam(':email',$email,PDO::PARAM_STR);
+						$SQL->bindParam(':date',$date,PDO::PARAM_STR);
+						$SQL->execute();
+						echo '<script>alert("Your account '.$login.' has been created!");</script>';
+						}catch(PDOEXCEPTION $e)
+						{
+							echo $e->getMessage();
+						}
+				}else{
+					return $Error;
+				}
 	}
+
 			
 	
 	public function get_Data($get)
